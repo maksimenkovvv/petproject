8df8ba3363e015d3512ac8a98d52276e155efeea
Anton Maksimenkov <urbangorod888@gmail.com>
2022-12-27 02:53:09 +0300

    finished petproject (99%)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // Настройки
-  columnWidth: 2000,
-  itemSelector: '.item'
-});
+let menuBtnOpen = document.querySelector('.menu__button');
+let menuBtnClose = document.querySelector('.modal__menu__close-btn');
+let menu = document.querySelector('.modal__menu__container')
+
+menuBtnOpen.addEventListener('click', function(){
+  menu.classList.toggle('active');
+})
+
+menuBtnClose.addEventListener('click', function(){
+  menu.classList.toggle('active');
+})
